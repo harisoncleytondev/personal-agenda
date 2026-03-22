eb79946ecd6e04091e3f11356b8074c47c854ada
harisoncleytondev <harisonc081@gmail.com>
2026-03-22 10:16:32 -0300

    feat: Adiciona link 'https://agenda.harisoncleyton.tech' nas configurações de cors

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -18,7 +18,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, 
+		AllowOrigins:     []string{"http://localhost:5173", "https://agenda.harisoncleyton.tech"}, 
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
